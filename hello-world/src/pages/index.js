e2c61c8c4e410d365b673847d0fbea07c9c37c9e
Yagmyr Musayev <yagmur.cw@gmail.com>
2019-01-22 15:01:33 -0800

    Tutorial3

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import { node } from "prop-types";
 
 export default () => (
     <div style={{ color: `purple` }}>
@@ -10,3 +11,14 @@ export default () => (
       <img src="https://source.unsplash.com/random/400x200" alt=""/>
     </div>
   )
+
+  function lockerRun(lockers) {
+      var run;
+      for (var i = 0; i<lockers.length; i ++) {
+if (lockers ===true) {
+
+}
+      }
+  } 
+
+ 
---
+++ tutorial-part-three/src/components/layout.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default ({ children }) => (
+  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>   
+    
+    <footer>
+  <p>Posted by: Yagmyr Musayev</p>
+  <p>Contact information: <a href="mailto:yagmur.cw@gmail.com">
+  yagmur.cw@gmail.com</a>.</p>
+</footer>
+
+  <h3>MySweetSite</h3> 
+    {children}
+  </div>
+)
